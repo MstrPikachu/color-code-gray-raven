d1593c3b5b9b6b95c5354156c9eb66c2aef768a8
MstrPikachu <31784486+MstrPikachu@users.noreply.github.com>
2024-11-20 01:06:15 -0500

    Hardcode string matching with plurals and singulars

--- content.js
+++ content.js
@@ -7,14 +7,35 @@
     );
     const mp = new Map();
 
-    spans.forEach(span => {
-        const str = span.textContent.trim();
-        if (!mp.has(str))
-            mp.set(str, mp.size);
-    });
+    const project = str => {
+        plural = str.charAt(str.length - 1) === "s";
+        ind = 0;
+        if (plural) {
+            for (let i = 0; i < str.length; i ++) {
+                if (str.charAt(i) < "0" || str.charAt(i) > "9") {
+                    ind = i;
+                    break;
+                }
+            }
+        }
+        else if (str.substr(0, 2) === "1 ") {
+            ind = 2;
+        }
+        proj = str.slice(ind, str.length - plural).trim();
+        return proj ? proj : str;
+    }
 
     spans.forEach(span => {
         const str = span.textContent.trim();
-        span.style.color = `hsl(${Math.floor(mp.get(str) * 360 / mp.size)}, 100%, 70%)`;
+        const proj = project(str);
+        if (!mp.has(proj))
+            mp.set(proj, mp.size);
+    });
+
+    console.log(mp);
+
+    spans.forEach(span => {
+        const str = span.textContent.trim();
+        span.style.color = `hsl(${Math.floor(mp.get(project(str)) * 360 / mp.size)}, 100%, 70%)`;
     });
 })();
